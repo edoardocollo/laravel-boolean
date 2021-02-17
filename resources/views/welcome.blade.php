00426713f7e99ed2c6329a14059030ab8817dfd9
edoardo <collo>
2021-02-17 13:51:27 +0100

    initial structure

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>home</h1>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <a href="/blog">blog</a>
+  </body>
+</html>
